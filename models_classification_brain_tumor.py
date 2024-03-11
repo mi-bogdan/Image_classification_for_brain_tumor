68bd38c8a578072b1ae67baa44ac546a60bf60c2
mi-bogdan <mi_shnyra@mail.ru>
2024-03-11 10:23:02 +0200

    Add code

--- models_classification_brain_tumor.py
+++ models_classification_brain_tumor.py
@@ -1,30 +1,33 @@
-import numpy as np
-from skimage.io import imread
-from skimage.transform import resize
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, recall_score, precision_score
-from tqdm import tqdm
 import os
+import time
+
 import cv2
 import joblib
-import time
+import numpy as np
 from prettytable import PrettyTable
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from tqdm import tqdm
 
 
 def load_images_and_labels(base_dir="Training"):
     """Загрузка изображений"""
-    categories = ["no_tumor", "glioma_tumor",
-                  "meningioma_tumor", "pituitary_tumor"]
+
+    categories = ["no_tumor", "glioma_tumor", "meningioma_tumor", "pituitary_tumor"]
     images = []
     labels = []
     count = 0
 
     for label, category in enumerate(categories):
         path = os.path.join(base_dir, category)
-        for img_name in tqdm(os.listdir(path), desc=f"Загрузка изображений из {category}"):
+        for img_name in tqdm(
+            os.listdir(path), desc=f"Загрузка изображений из {category}"
+        ):
             img_path = os.path.join(path, img_name)
             img = imread(img_path, as_gray=True)
             img = resize(img, (128, 128), anti_aliasing=True)
@@ -38,6 +41,7 @@ def load_images_and_labels(base_dir="Training"):
 
 def aggregate_descriptors(descriptors_list):
     """Аггрегация дескрипторов каждого изображения с использованием среднего значения"""
+
     agg_descriptors = []
     for des in descriptors_list:
         if des is not None:
@@ -55,11 +59,12 @@ def extract_sift_descriptors(images):
     descriptors_list = []
 
     for img in tqdm(images, desc="Извлечение дескрипторов SIFT"):
-        kp, des = sift.detectAndCompute((img*255).astype('uint8'), None)
+        kp, des = sift.detectAndCompute((img * 255).astype("uint8"), None)
         if des is not None:
             descriptors_list.append(des)
 
     # Аггрегация дескрипторов каждого изображения
+
     agg_descriptors = aggregate_descriptors(descriptors_list)
 
     return agg_descriptors
@@ -73,8 +78,9 @@ def evaluate_model(model, X_test, y_test):
     predict_time = time.time() - start_time
 
     accuracy = accuracy_score(y_test, predictions)
-    recall = recall_score(y_test, predictions, average='weighted')
-    precision = precision_score(y_test, predictions, average='weighted')
+
+    recall = recall_score(y_test, predictions, average="weighted")
+    precision = precision_score(y_test, predictions, average="weighted")
 
     return accuracy, recall, precision, predict_time
 
@@ -84,20 +90,19 @@ agg_descriptors = extract_sift_descriptors(images)
 
 # Разделяем данные на обучающую и тестовую выборки
 X_train, X_test, y_train, y_test = train_test_split(
-    agg_descriptors, labels, test_size=0.3, random_state=42)
+    agg_descriptors, labels, test_size=0.3, random_state=42
+)
 
 # Обучение классификатора SVM
-svm_clf = make_pipeline(StandardScaler(), SVC(kernel='rbf'))
+svm_clf = make_pipeline(StandardScaler(), SVC(kernel="rbf"))
 svm_clf.fit(X_train, y_train)
 
 # Сохранение модели
-joblib.dump(svm_clf, 'svm_clf.joblib')
-model_size = os.path.getsize('svm_clf.joblib') / \
-    (1024 * 1024)  # Размер в Мегабайтах
+joblib.dump(svm_clf, "svm_clf.joblib")
+model_size = os.path.getsize("svm_clf.joblib") / (1024 * 1024)  # Размер в Мегабайтах
 
 # Оценка модели на тестовом наборе
-accuracy, recall, precision, predict_time = evaluate_model(
-    svm_clf, X_test, y_test)
+accuracy, recall, precision, predict_time = evaluate_model(svm_clf, X_test, y_test)
 
 # Выводим результаты
 table = PrettyTable()
